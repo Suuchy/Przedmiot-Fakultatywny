e318df6ea436526d7caa9f9891bd4b46341490f4
Suuchy <47736285+Suuchy@users.noreply.github.com>
2021-04-26 18:19:53 +0200

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,52 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route, BrowserRouter } from 'react-router-dom'; 
+
+import Home from './views/home/Home'
+import Movie from './views/movie/Movie'
+import Page404 from './views/page404/Page404';
+
 import './App.css';
 
-import CustomHeader, {addNumber} from './components/CustomHeader'; 
-import Counter from './components/Counter';
-import Counter1 from './components/counter1';
+const App = () => {
 
-
-// waze aby jeden komponent zwracal jeden div maksymalnie
-const App = () =>{
-
-  const addedNumber = addNumber(1,2);
-  // aby 
-
-  //const handleNumberChange = (newNumber: number) => {
-    //return newNumber;
-
-    //changednumber bedzie sie zmienial wedlug setchangednumber
-  const[changedNumber, setChangedNumber] = React.useState(0);
-
-  const handleNumberChange = (newNumber: number) => {
-    setChangedNumber(newNumber);
-  }
-
-  const liczbaMniejszaOdZera = (liczba: number)=> {
-    if(liczba<0){
-      return (<div>liczba jest mniejsza od 0</div>)
-    }
-  }
-
-  return (
+  return(
     <div className="App">
-      <CustomHeader />
-      <Counter />
-      <Counter1 initialNumber={10} />
-
-      <Counter1 onNumberChange={handleNumberChange} />
-      {/*conditional rendering* tak lub. przez utworzenie funkcji*/}
-      {
-        changedNumber > 0 && (<div> liczba jest wieksza od 0</div>)
-      }
-      
-      {liczbaMniejszaOdZera(changedNumber)}
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movie/:id" component={Movie} />
+          <Route path="/" component={Home} exact/>
+          <Route path="*" component={Page404} />
+        </Switch>
+      </BrowserRouter>
 
     </div>
   );
-}
-//<Counter1 onNumberChange={(newNumber)=> {console.log(newNumber)}} />
+} 
 export default App;
 
 
